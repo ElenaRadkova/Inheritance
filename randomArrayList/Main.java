7acef218572b5a9292cff31dd22961b3c31f6d87
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2023-09-18 19:16:02 +0300

    Add new tasks
    Add a new exercises in Inheritance-Lab

---
+++ randomArrayList/Main.java
@@ -0,0 +1,10 @@
+package randomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArray = new RandomArrayList();
+        randomArray.getRandomElement();
+
+
+    }
+}
